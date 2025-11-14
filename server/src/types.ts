497686ec083548ca3aaea6dc896a415afdb3474b
gadishiimwe <gadyishimwe1@gmail.com>
2025-11-15 00:05:04 +0200

    Existing

--- server/src/types.ts
+++ server/src/types.ts
@@ -17,3 +17,16 @@ export type GetTypesResult =
 | { type: 'p1', p2id: string | null }
 | { type: 'p2', p1id: string | null }
 | false;
+
+export interface RoomParticipant {
+  socketId: string;
+  userInfo?: { name: string, age?: number, sex?: string } | null;
+  userId?: string | null;
+}
+
+export interface CustomRoom {
+  roomCode: string;
+  participants: RoomParticipant[];
+  createdAt: Date;
+  maxUsers: number;
+}
